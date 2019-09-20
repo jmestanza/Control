500010e9f8a3867e869aae3288f3ca8353c8e827
Joaquin Mestanza <jmestanza@itba.edu.ar>
2019-09-20 03:55:24 -0300

    falta desarrollar

--- TP1/transf1erfilt.m
+++ TP1/transf1erfilt.m
@@ -14,9 +14,9 @@ Vcc = 10;
 Kd = 10 / pi;
 
 % coef amortiguamiento con filtro 1er orden
-xi = (C6*Kd*Ko*R6 + 1)/(2*C6^(1/2)*Kd^(1/2)*Ko^(1/2)*(R5 + R6)^(1/2));
+xi = (C6*Kd*Ko*R6 + 1)/(2*C6^(1/2)*Kd^(1/2)*Ko^(1/2)*(R5 + R6)^(1/2))
 % frecuencia natural de los polos complejos
-wn = ((Kd*Ko)/(C6*(R5 + R6)))^(1/2);
+wn = ((Kd*Ko)/(C6*(R5 + R6)))^(1/2)
 s = tf('s');
 sys = (Kd*Ko)/((C6*R5*s + 1)*(C7*R9*s + 1)*(s + (Kd*Ko)/(C6*R5*s + 1)));
 step(sys);
