9aaf95c210c9e6d57fbcb21218396491724f3830
rocioparra <rparra@itba.edu.ar>
2019-09-25 14:17:23 -0300

    xy plot formatting

--- TP2/grafiarXY.m
+++ TP2/grafiarXY.m
@@ -6,6 +6,7 @@ v2sim = ScopeData.signals(2).values(103:1000,1);
 
 % agrego la carpeta donde estan las mediciones al path
 addpath('C:\Users\Marcelo\Documents\GitHub\Control\TP2\Mediciones');
+% addpath('C:\repositories\Control\TP2\Mediciones');
 f = csvread('faseNormal.csv',2,0); % ignoro los primeros 2 renglones 
 v1med = f(1:999,5);
 v2med = f(1:999,6);
@@ -13,3 +14,18 @@ v2med = f(1:999,6);
 plot(v2sim, v1sim);
 hold on;
 plot(v1med, v2med);
+
+
+titlestr = '';
+xlabelstr = '$X_1$';
+ylabelstr = '$X_2$';
+legendstr = {'Simulaci\''on', 'Medici\''on'};
+ax = gca;
+title(titlestr, 'interpreter', 'latex');
+set(ax,'TickLabelInterpreter','latex');
+grid on;
+
+xlabel(xlabelstr,'interpreter','latex');
+ylabel(ylabelstr,'interpreter','latex');
+lgd = legend(legendstr);
+lgd.Interpreter = 'latex';
