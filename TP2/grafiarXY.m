f49b1d529baaac6c6f2e59fc5ddfeb34e82b4bfa
rocioparra <rparra@itba.edu.ar>
2019-09-25 14:29:54 -0300

    xy-sin-k corrected

--- TP2/grafiarXY.m
+++ TP2/grafiarXY.m
@@ -6,10 +6,10 @@ v2sim = ScopeData.signals(2).values(103:1000,1);
 
 % agrego la carpeta donde estan las mediciones al path
 addpath('C:\Users\Marcelo\Documents\GitHub\Control\TP2\Mediciones');
-% addpath('C:\repositories\Control\TP2\Mediciones');
-f = csvread('faseNormal.csv',2,0); % ignoro los primeros 2 renglones 
-v1med = f(1:999,5);
-v2med = f(1:999,6);
+addpath('C:\repositories\Control\TP2\Mediciones');
+f = csvread('FaseEspiral.csv',2,0); % ignoro los primeros 2 renglones 
+v1med = f(1:999,2);
+v2med = f(1:999,5);
 
 plot(v2sim, v1sim);
 hold on;
